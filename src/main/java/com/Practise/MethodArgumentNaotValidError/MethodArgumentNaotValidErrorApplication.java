4066e4b69a6dd5e53ad9e280c494ec8cb1915c59
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-31 22:47:22 +0530

    Deployment

---
+++ src/main/java/com/Practise/MethodArgumentNaotValidError/MethodArgumentNaotValidErrorApplication.java
@@ -0,0 +1,13 @@
+package com.Practise.MethodArgumentNaotValidError;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethodArgumentNaotValidErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MethodArgumentNaotValidErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practise/MethodArgumentNaotValidError/MethodArgumentNaotValidErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practise.MethodArgumentNaotValidError;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethodArgumentNaotValidErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
